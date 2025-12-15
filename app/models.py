a3ea0717afa734d4d8e7c64b4c29153fd3191159
Jeevan Krishna <jeevanbaddiga143@gmail.com>
2025-12-15 12:32:37 +0530

    fix pytest using python module

---
+++ app/models.py
@@ -0,0 +1,7 @@
+# Models will be added later (DB integration)
+# Keeping this file for clean structure
+
+class User:
+    def __init__(self, username, role):
+        self.username = username
+        self.role = role
